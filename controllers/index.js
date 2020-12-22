ed5723de7ae84c088e08c6ed85e8cb3dc620f450
Francis Roy Balasabas <francisroy1124@gmail.com>
2020-12-21 21:04:38 -0500

    changed movies.js
    changes with isha

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-var router = require("express").Router();
-var movieController = require('./movieControllers');
-var htmlController = require('./pageControllers')
-
-router.use('/movies', movieController)
-router.use(htmlController)
-
-// Export routes for server.js to use.
-module.exports = router;
